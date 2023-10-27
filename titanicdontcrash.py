7d5fc54834404f6794606cf8ea9cb7e01c1c5660
wheatgreaser <108815657+wheatgreaser@users.noreply.github.com>
2023-10-27 17:42:37 +0530

    Add files via upload

---
+++ titanicdontcrash.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+from sklearn import svm
+from sklearn.model_selection import train_test_split
+
+data = pd.read_csv('train.csv')
+
+X = data["Sex"].head(50)
+y = data["Survived"].head(50)
+
+
+encX = pd.get_dummies(X)
+print(encX)
+model = svm.SVC()
+model.fit(encX, y)
